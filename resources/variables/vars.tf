78f80c420db0dd04000e082094599c3d60b53412
shree <shreesbadiger@gmail.com>
2023-12-12 23:58:55 +0530

    updated

--- resources/variables/vars.tf
+++ resources/variables/vars.tf
@@ -1,14 +1,18 @@
-variable "sample1" {
+variable "sample1"{
     default = "hello"
 } 
-
 output "sample1"{
     value = var.sample1
 }
-
 variable "fruits"{
     default = ["apple","mango"]
 }
- output "list_of_fruits" {
+ output "list_of_fruits"{
     value = var.fruits
- }
+}
+variable "fruits"{
+    default=["apple","mango"]
+}
+output "first_fruit_is" {
+    value = var.fruits[0]
+}
