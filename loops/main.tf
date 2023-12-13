03dedfe5a63f17074e9d0dc46515bed8ad0ea7ff
shree <shreesbadiger@gmail.com>
2023-12-13 22:33:29 +0530

    updated

--- loops/main.tf
+++ loops/main.tf
@@ -4,3 +4,23 @@ variable "instances" {
 resource "null_resource" "sample"{
     count = length(var.instances)
 }
+
+variable "instances1" "sample1" {
+    default = {
+        frontend = {
+            Name = "frontend"
+            instance_type = "t3.micro"
+        }
+        Backend = {
+            Name = "backend"
+            instance_type = "t3.micro"
+        }
+        mysql = {
+            Name = " mysql"
+            instance_type = "t3.micro"
+        }
+    }      
+}
+resource "null_resource" "sample1"{
+    for_each = var.instances1
+}
