acc1546c726fbf918bab2bfcd14ec3b93781895f
shree <shreesbadiger@gmail.com>
2023-12-18 01:45:04 +0530

    updated

--- data-src/main.tf
+++ data-src/main.tf
@@ -1,6 +1,6 @@
-data "aws_ssm_parameter" "username" {
-  name = "dev.rds.username"
-}
-data "aws_ssm_parameter" "password" {
-  name = "dev.rds.password"
-}
+#data "aws_ssm_parameter" "username" {
+  #name = "dev.rds.username"
+#}
+#data "aws_ssm_parameter" "password" {
+  #name = "dev.rds.password"
+#}
